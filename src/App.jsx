75dc6753e9dde3bbe0e1df7d6b722ff6b69a0309
DariaAleshina <ddaleshina@gmail.com>
2025-04-08 13:28:25 +0200

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,14 @@ const INITIAL_STATE = {
   expectedReturn: 6,
   duration: 10,
 };
+
+const ERROR_MESSAGE =
+  'Please make sure duration period is at least 1 year and more';
+
 function App() {
   const [state, setStateUpdate] = useState(INITIAL_STATE);
+
+  const isInputValid = state.duration >= 1;
 
   const handleInputChange = function (field, value) {
     if (value < 0) return;
@@ -24,7 +30,8 @@ function App() {
   return (
     <>
       <InputContainer initialInput={state} onInputChange={handleInputChange} />
-      <OutputTable initialInput={state} />
+      {isInputValid && <OutputTable initialInput={state} />}
+      {!isInputValid && <p className="center">{ERROR_MESSAGE}</p>}
     </>
   );
 }
